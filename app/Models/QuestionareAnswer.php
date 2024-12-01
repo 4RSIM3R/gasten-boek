ca4810dba43ff1fa7951d28eb57d2587fc7ac601
unknown <ilzammulkhaq85@gmail.com>
2024-12-01 20:32:50 +0700

    :tada: initial commit, bismillah

---
+++ app/Models/QuestionareAnswer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionareAnswer extends Model
+{
+    /** @use HasFactory<\Database\Factories\QuestionareAnswerFactory> */
+    use HasFactory;
+}
