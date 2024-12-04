8b68f7d06fb712689f4dfe428ee70b01088e3e61
unknown <ilzammulkhaq85@gmail.com>
2024-12-04 15:15:16 +0700

    fix: name for final

--- resources/js/pages/questionare.tsx
+++ resources/js/pages/questionare.tsx
@@ -1,8 +1,19 @@
-export default function Questionare() {
+import { Questionare } from "@/types/questionare"
+import { useForm } from "@inertiajs/react"
+
+type QuestionareProps = {
+    question: Questionare[],
+}
+
+export default function QuestionareForm({ question }: QuestionareProps) {
+
+    const { data, setData, errors, processing } = useForm();
 
     return (
-        <div>
-            
+        <div className="max-w-5xl mx-auto p-8" >
+            <form action="">
+
+            </form>
         </div>
     )
 
